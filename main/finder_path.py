7dd359c323fe5d3b11ff5d2e48c9af3c088bb372
Victor Castañeda <vidanilo10@gmail.com>
2021-12-08 11:35:02 -0500

    Finde peak and bottom

--- main/finder_path.py
+++ main/finder_path.py
@@ -24,7 +24,35 @@ class PathHandler:
 
     def Handler(data: Dict):
         graph = Graph().build_graph(data=data)
-        print(graph)
+        peak = Peaks().find_peak(G=graph)
+        bottom = Bottom().find_bottom(G=graph)
+        
+
+class Peaks:
+    def __init__(self) -> None:
+        self.maxpt = 0
+
+    def find_peak(self, G):
+        for coord in G:
+            if G[coord] > self.maxpt:
+                self.maxpt = G[coord]
+                maxcoord = coord
+        return maxcoord        
+
+class Bottom:
+    def __init__(self) -> None:
+        self.mincoord = 0, 0
+
+    def find_bottom(self, G):
+        minpt = G[0, 0]
+        
+        for coord in G:
+            if G[coord] < minpt:
+                minpt = G[coord]
+                self.mincoord = coord
+
+        return self.mincoord
+
 
 if __name__ == "__main__":
     PathHandler().Handler()
